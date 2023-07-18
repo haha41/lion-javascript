01d6e5c5a92663ad9e4c7fc4c717297eb43d4f46
haha41 <thgml500@gmail.com>
2023-07-18 22:33:23 +0900

    getNode의 능력

--- client/lib/dom/getNode.js
+++ client/lib/dom/getNode.js
@@ -1,3 +1,6 @@
+// 문자를 받아서 querySelector로 돌려서 element로 반환
+
+// '.first'
 function getNode(node){
   if(typeof node !== 'string'){
     throw new Error ('getNode 함수의 인수는 문자 타입이어야 합니다.')
@@ -6,8 +9,6 @@ function getNode(node){
 }
 
 // 여러 요소를 잡을 경우
-
-// getNodes
 function getNodes(node){
   if(typeof node !== 'string'){
     throw new Error ('getNode 함수의 인수는 문자 타입이어야 합니다.')
