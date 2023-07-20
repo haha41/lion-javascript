4529c6077d1e9ed426f71adf7d274ceb08ad0a76
haha41 <thgml500@gmail.com>
2023-07-20 15:43:45 +0900

    뭐야

--- client/main.js
+++ client/main.js
@@ -1,7 +1,102 @@
-/* eslint no-unused-vars: 1 */
+// 모듈 프로그래밍 => js 의 어려운 문법은 x 방법론
 
-console.log('hello js!');
+// getNode 가져오기
 
-const a = 10;
+// 번들러의 힘
+// webpack vite
 
-const b = 10;
+import { getNode, clearContents, insertLast } from './lib/index.js';
+
+
+
+// [page-1]
+// 1. input value 값 가져오기
+// 2. 두 수의 합 더하기
+// 3. 화면 출력 하기
+const first = getNode('#firstNumber'); // 첫 번째 인풋
+const second = getNode('#secondNumber'); // 두 번째 인풋
+const result = getNode('.result');
+
+function handleInput(){
+  // let firstValue = Number(first.value); // 10 + 10 = 1010 안되게 숫자로 변환
+  let firstValue = +first.value; // 암시적 형변환
+  let secondtValue = second.value / 1;
+  let total = firstValue + secondtValue;
+
+  clearContents(result);
+  insertLast(result, total);
+
+}
+
+// [page-2]
+// clear 버튼을 누르면 모든 글자가 초기화 될 수 있도록 만들어 주세요
+
+// 1. clear 버튼을 가져온다.
+const clear = getNode('#clear')
+
+
+
+function handleClear(){
+  // 3. firstValue 값은 지운다.
+  clearContents(first)
+
+  // 4. lastValue 값은 지운다.
+  clearContents(second)
+
+  // 5. result의 값을 지운다.
+  clearContents(result)
+
+  // 6. result에 - 값을 넣는다.
+  result.textContent = '-'
+}
+
+// 2. clear 버튼에 이벤트 핸들러를 연결한다.
+clear.addEventListener('click',handleClear)
+
+
+
+
+
+first.addEventListener('input',handleInput)
+second.addEventListener('input',handleInput)
+
+
+
+// [page-3]
+// 위 내용을 이벤트 위임으로 변경
+// .calculator 이벤트 input
+/* function page2(){
+
+
+  const calculator = getNode('.calculator');
+  const clear = getNode('#clear');
+  const result = getNode('.result');
+  const numberInputs = Array.from(
+    getNodes('input:not(#clear)')
+  )
+  
+  console.log( numberInputs );
+  
+  
+  function handleInput(){
+    
+    const total = numberInputs.reduce((total,input)=> total + Number(input.value),0)
+  
+  
+    console.log(  );
+  
+    clearContents(result);
+    insertLast(result,total);
+    
+  }
+  
+  function handleClick(){
+  
+  
+    numberInputs.forEach(clearContents);
+    result.textContent = '-'
+  }
+  
+  calculator.addEventListener('input',handleInput);
+  clear.addEventListener('click',handleClick);
+  } */
