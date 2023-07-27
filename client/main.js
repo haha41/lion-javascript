e60e1bfdac3f4ed777e9d92ab9e36cb04b7f1a9e
haha41 <thgml500@gmail.com>
2023-07-27 09:15:33 +0900

    fetch

--- client/main.js
+++ client/main.js
@@ -1,8 +1,18 @@
 
+import { insertLast, tiger } from "./lib/index.js";
 
-import { tiger } from "./lib/index.js";
 
 
-const data = await tiger.get('https://jsonplaceholder.typicode.com/users')
+const response = await fetch(URL);
+const data = await response.json();
 
-console.log(data);
+console.log( data );
+
+// fetch(URL).then((result)=>{
+
+//     result // response object
+//     return result.json()
+// })
+// .then((result)=>{
+//   console.log( result );
+// })
